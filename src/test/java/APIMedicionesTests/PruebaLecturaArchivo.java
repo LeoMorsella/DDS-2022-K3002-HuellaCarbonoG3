c1f55b62e20e110746603ba264e3cc10bfec5dcc
unknown <leomorsella@frba.utn.edu.ar>
2022-08-27 15:39:35 -0300

    se generan excel de prueba con test de carga

--- src/test/java/APIMedicionesTests/PruebaLecturaArchivo.java
+++ src/test/java/APIMedicionesTests/PruebaLecturaArchivo.java
@@ -41,12 +41,49 @@ public class PruebaLecturaArchivo {
 
     @Test
     public void testCargarDatosALista() {
-        String filePath = "..\\DDS-2022-K3002-Grupo_3\\mediciones1.xlsx";
+        String filePath = "..\\DDS-2022-K3002-HuellaCarbonoG3\\mediciones1.xlsx";
         CargaDeMediciones cargaMediciones = new CargaDeMediciones();
         cargaMediciones.useExistingWorkbook(filePath);
         List<DatoDeMedicion> listaDatoDeMedicion = cargaMediciones.lecturaArchivo(0);
-        Assert.assertEquals(845.0,listaDatoDeMedicion.get(2).getValor());
+        Assert.assertEquals(1400.0,listaDatoDeMedicion.get(2).getValor());
     }
+
+    @Test
+    public void testCargarDatosALista2() {
+        String filePath = "..\\DDS-2022-K3002-HuellaCarbonoG3\\mediciones2.xlsx";
+        CargaDeMediciones cargaMediciones = new CargaDeMediciones();
+        cargaMediciones.useExistingWorkbook(filePath);
+        List<DatoDeMedicion> listaDatoDeMedicion = cargaMediciones.lecturaArchivo(0);
+        Assert.assertEquals(750.0,listaDatoDeMedicion.get(3).getValor());
+    }
+
+    @Test
+    public void testCargarDatosALista3() {
+        String filePath = "..\\DDS-2022-K3002-HuellaCarbonoG3\\mediciones3.xlsx";
+        CargaDeMediciones cargaMediciones = new CargaDeMediciones();
+        cargaMediciones.useExistingWorkbook(filePath);
+        List<DatoDeMedicion> listaDatoDeMedicion = cargaMediciones.lecturaArchivo(0);
+        Assert.assertEquals(1564.0,listaDatoDeMedicion.get(2).getValor());
+    }
+
+    @Test
+    public void testCargarDatosALista4() {
+        String filePath = "..\\DDS-2022-K3002-HuellaCarbonoG3\\mediciones4.xlsx";
+        CargaDeMediciones cargaMediciones = new CargaDeMediciones();
+        cargaMediciones.useExistingWorkbook(filePath);
+        List<DatoDeMedicion> listaDatoDeMedicion = cargaMediciones.lecturaArchivo(0);
+        Assert.assertEquals(750.0,listaDatoDeMedicion.get(2).getValor());
+    }
+
+    @Test
+    public void testCargarDatosALista5() {
+        String filePath = "..\\DDS-2022-K3002-HuellaCarbonoG3\\mediciones5.xlsx";
+        CargaDeMediciones cargaMediciones = new CargaDeMediciones();
+        cargaMediciones.useExistingWorkbook(filePath);
+        List<DatoDeMedicion> listaDatoDeMedicion = cargaMediciones.lecturaArchivo(0);
+        Assert.assertEquals(1450.0,listaDatoDeMedicion.get(2).getValor());
+    }
+
 
     @Test
     public void calcularHuellaMiembro() {
