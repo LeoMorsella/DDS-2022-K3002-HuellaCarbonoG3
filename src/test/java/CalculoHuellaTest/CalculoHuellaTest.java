560f57fe900449336334a68bc6dd98affa3ad153
unknown <leomorsella@frba.utn.edu.ar>
2022-09-08 18:31:28 -0300

    .

--- src/test/java/CalculoHuellaTest/CalculoHuellaTest.java
+++ src/test/java/CalculoHuellaTest/CalculoHuellaTest.java
@@ -22,23 +22,15 @@ public class CalculoHuellaTest {
 
     @Test
     public void calcularHuellaMiembro() throws Exception {
-        CargarDatos.cargarFE();
-        Calendar mesInicio = Calendario.crearFecha(0, 2022);
-        Calendar mesFin = Calendario.crearFecha(11, 2022);
         Ubicacion ubicacion = CargarDatos.cargarUbicacion1();
-        ArrayList<Area> areas = new ArrayList<>();
-        ArrayList<Recorrido> recorridos = new ArrayList<>();
-        ArrayList<Miembro> miembros = new ArrayList<>();
-        Organizacion organizacion = new Organizacion("Panini", TipoOrg.EMPRESA,ubicacion,Clasificacion.EMPRESA_SECTOR_PRIMARIO,miembros,miembros);
-        Recorrido recorrido = new Recorrido(organizacion,1.0,mesInicio,mesFin);
-        recorridos.add(recorrido);
-        Miembro miembro = new Miembro("Juan","Perez","DNI",123456,areas,recorridos);
-        miembros.add(miembro);
-
-        Area area = new Area("CABA",organizacion);
-        areas.add(area);
-        recorridos.add(recorrido);
-        miembros.add(miembro);
+        Area area = CargarDatos.cargarArea1();
+        Recorrido recorrido = CargarDatos.cargarRecorrido1();
+        Miembro miembro = CargarDatos.cargarMiembro1();
+        miembro.addRecorrido(recorrido);
+        Organizacion organizacion = CargarDatos.cargarOrganizacion1();
+        organizacion.addArea(area);
+        Calendar mesInicio = Calendario.crearFecha(10, 2022);
+        Calendar mesFin = Calendario.crearFecha(11, 2022);
 
         Double huella = CalculadoraHCService.getCalculadoraHC().calcularHCMiembro(miembro, mesInicio, mesFin, organizacion);
 
@@ -46,30 +38,7 @@ public class CalculoHuellaTest {
     }
 
     @Test
-
-    //Creeria que este test cumple
     public void calcularHuellaOrganizacion() throws Exception {
-        //Carga de datos manual
-        /*
-        Calendar mesInicio = Calendario.crearFecha(0, 2022);
-        Calendar mesFin = Calendario.crearFecha(11, 2022);
-        Ubicacion ubicacion = CargarDatos.cargarUbicacion1();
-        ArrayList<Area> areas = new ArrayList<>();
-        ArrayList<Recorrido> recorridos = new ArrayList<>();
-        ArrayList<Miembro> miembros = new ArrayList<>();
-        Organizacion organizacion = new Organizacion("Panini", TipoOrg.EMPRESA,ubicacion,Clasificacion.EMPRESA_SECTOR_PRIMARIO,miembros,miembros);
-        Recorrido recorrido = new Recorrido(organizacion,1.0,mesInicio,mesFin);
-        recorridos.add(recorrido);
-        Miembro miembro = new Miembro("Juan","Perez","DNI",123456,areas,recorridos);
-        miembros.add(miembro);
-
-        Area area = new Area("CABA",organizacion);
-        areas.add(area);
-        recorridos.add(recorrido);
-        miembros.add(miembro);
-*/
-        //Carga de datos por cargaDatos
-
         Ubicacion ubicacion = CargarDatos.cargarUbicacion1();
         Area area = CargarDatos.cargarArea1();
         Recorrido recorrido = CargarDatos.cargarRecorrido1();
@@ -91,31 +60,12 @@ public class CalculoHuellaTest {
 
         Double huella = CalculadoraHCService.getCalculadoraHC().calcularHCOrganizacion(organizacion, mesInicio, mesFin);
 
-        //Assert.assertEquals(Optional.of(1.1), huella);
-        Assertions.assertEquals(16.666666666666668,huella);
-        //Assert.assertNotEquals(Optional.of(0.0), huella);
+        //Assertions.assertEquals(16.666666666666668,huella);
+        Assertions.assertNotEquals(0,huella);
     }
 
     @Test
     public void calcularHuellaArea() throws Exception {
-/*
-        Calendar mesInicio = Calendario.crearFecha(0, 2022);
-        Calendar mesFin = Calendario.crearFecha(11, 2022);
-        Ubicacion ubicacion = CargarDatos.cargarUbicacion1();
-        ArrayList<Area> areas = new ArrayList<>();
-        ArrayList<Recorrido> recorridos = new ArrayList<>();
-        ArrayList<Miembro> miembros = new ArrayList<>();
-        Organizacion organizacion = new Organizacion("Panini", TipoOrg.EMPRESA,ubicacion,Clasificacion.EMPRESA_SECTOR_PRIMARIO,miembros,miembros);
-        Recorrido recorrido = new Recorrido(organizacion,1.0,mesInicio,mesFin);
-        recorridos.add(recorrido);
-        Miembro miembro = new Miembro("Juan","Perez","DNI",123456,areas,recorridos);
-        miembros.add(miembro);
-
-        Area area = new Area("CABA",organizacion);
-        areas.add(area);
-        recorridos.add(recorrido);
-        miembros.add(miembro);
-*/
 
         Ubicacion ubicacion = CargarDatos.cargarUbicacion2();
         Area area = CargarDatos.cargarArea1();
@@ -136,21 +86,24 @@ public class CalculoHuellaTest {
 
         Double huella = CalculadoraHCService.getCalculadoraHC().calcularHCArea(area, mesInicio, mesFin);
 
-        //Assert.assertEquals(Optional.of(1.1),huella);
-        Assertions.assertEquals(49.99999999999999,huella);
+
+
+        Assertions.assertNotEquals(0,huella);
     }
 
     //No toma los datos de organizacioon
     @Test
     public void calcularHuellaSectorTerritorial() throws Exception{
         CargarDatos.cargarFE();
-        Calendar mesInicio = Calendario.crearFecha(0, 2022);
+        Calendar mesInicio = Calendario.crearFecha(2, 2022);
         Calendar mesFin = Calendario.crearFecha(11, 2022);
         SectorTerritorial sector = CargarDatos.cargarSector1();
         Organizacion organizacion = CargarDatos.cargarOrganizacion1();
-        Organizacion organizacion2 = CargarDatos.cargarOrganizacion5();
+
+        Area area = CargarDatos.cargarArea1();
+        organizacion.addArea(area);
+
         sector.getOrganizaciones().add(organizacion);
-        sector.getOrganizaciones().add(organizacion2);
 
         Double huella = CalculadoraHCService.getCalculadoraHC().calcularHCSectorTerritorial(sector, mesInicio, mesFin);
     
