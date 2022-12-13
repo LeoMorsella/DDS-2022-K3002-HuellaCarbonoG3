580ca863739559431f4b8c2e49dd831c264d876f
Luca <ldelpieri@frba.utn.edu.ar>
2022-12-13 04:49:47 -0300

    Update Miembro.java

--- src/main/java/utn/frba/huelladecarbono/model/ModeloDeNegocio/Miembro.java
+++ src/main/java/utn/frba/huelladecarbono/model/ModeloDeNegocio/Miembro.java
@@ -17,7 +17,7 @@ import java.util.stream.Collectors;
 @Table(name = "miembro")
 public class Miembro {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @Column
     private String nombre;
