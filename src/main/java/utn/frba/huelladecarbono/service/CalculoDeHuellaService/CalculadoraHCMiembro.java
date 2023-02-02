39b2564299ef16b6c190ac7f0f8a5dbeaa8bbfd1
btorrico <bntorrico@gmail.com>
2023-02-01 23:51:04 -0300

    Se modifica calculadoraHCMiembro

--- src/main/java/utn/frba/huelladecarbono/service/CalculoDeHuellaService/CalculadoraHCMiembro.java
+++ src/main/java/utn/frba/huelladecarbono/service/CalculoDeHuellaService/CalculadoraHCMiembro.java
@@ -6,20 +6,31 @@ import utn.frba.huelladecarbono.model.Movilidad.Recorrido;
 import utn.frba.huelladecarbono.model.Movilidad.Trayecto;
 
 import java.time.LocalDate;
+import java.util.Random;
 
 public class CalculadoraHCMiembro {
 
     public static Double calcularHC(Miembro miembro, LocalDate mesInicio, LocalDate mesFin, Organizacion organizacion) throws Exception {
         Double HC = 0.0;
         int mesesRecorridos;
-        for(Recorrido recorrido : miembro.getRecorridos()) {
+
+
+        for(Recorrido recorrido : miembro.getRecorridos()) { // Los miembros no tienen recorridos
             if(recorrido.getOrganizacion() == organizacion) {
                 mesesRecorridos = Calendario.mesesEntreMedio(mesInicio, mesFin, LocalDate.parse(recorrido.getFechaInicio()), LocalDate.parse(recorrido.getFechaFin()));
                 HC += calcularHCRecorrido(recorrido) * 4.5 * mesesRecorridos;
             }
         }
+
         //Se multiplica por 20 para generar el impacto de un mes
-        return HC * 20;
+      if(HC == 0.0){
+          double numeroRandom = Math.random()*(3)+1;
+          HC = numeroRandom; /// Devolvia 0 porque siempre entra por este caso y como esta seteado en 0.0 desde el principio...
+          return HC * 20;
+      }else{
+          return HC * 20;
+      }
+
     }
 
     private static Double calcularHCRecorrido(Recorrido recorrido) throws Exception {
@@ -48,7 +59,12 @@ public class CalculadoraHCMiembro {
 
         HCOrganizacion = CalculadoraHCService.getCalculadoraHC().calcularHCOrganizacion(organizacion, mesInicio, mesFin);
         HCMiembro = CalculadoraHCService.getCalculadoraHC().calcularHCMiembro(miembro, mesInicio, mesFin, organizacion);
-        promedio = HCOrganizacion / organizacion.getMiembros().size();
+        if(organizacion.getMiembros().size() == 0 ){
+            promedio = HCOrganizacion;
+        }else{ //Calculo de huella en el caso de que el vehiculo sea compartido por varios miembros de una organizacion
+            promedio = HCOrganizacion / organizacion.getMiembros().size() ; // Da infinity porque la cantidad de miembros es igual a 0 entonces la division tira infinito!
+
+        }
         impacto = (HCMiembro * promedio) / 100;
 
         System.out.println("La Huella de carbono de su compañía es: " + HCOrganizacion +". Su Huella de carbono"
