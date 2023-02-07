8a66842ca1452b932c63045665cbf89a9b882723
Luca <ldelpieri@frba.utn.edu.ar>
2023-02-06 21:19:57 -0300

    Elimino SC

--- src/main/java/utn/frba/huelladecarbono/service/IUsuarioService.java
+++ src/main/java/utn/frba/huelladecarbono/service/IUsuarioService.java
@@ -1,13 +1,6 @@
 package utn.frba.huelladecarbono.service;
 
 
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-import utn.frba.huelladecarbono.model.ModeloDeNegocio.Organizacion;
-import utn.frba.huelladecarbono.model.Seguridad.Usuario;
-
-import java.util.List;
-
-public interface IUsuarioService extends UserDetailsService {
+public interface IUsuarioService {
 
 }
--- src/main/java/utn/frba/huelladecarbono/service/UsuarioService.java
+++ src/main/java/utn/frba/huelladecarbono/service/UsuarioService.java
@@ -1,8 +1,6 @@
 package utn.frba.huelladecarbono.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import utn.frba.huelladecarbono.model.Seguridad.Usuario;
 import utn.frba.huelladecarbono.repository.UsuarioRepository;
@@ -17,9 +15,4 @@ public class UsuarioService implements IUsuarioService {
         return usuarioRepository.findByUsername(username);
     }
 
-
-    @Override
-    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
-    }
 }
