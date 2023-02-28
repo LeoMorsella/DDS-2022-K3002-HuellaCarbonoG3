1d86055b5b87e7c35b1a2653f2812c38dc3ec7be
Luca <ldelpieri@frba.utn.edu.ar>
2023-02-27 22:08:52 -0300

    unos arreglos

--- src/main/java/utn/frba/huelladecarbono/DTO/ResRecorrido.java
+++ src/main/java/utn/frba/huelladecarbono/DTO/ResRecorrido.java
@@ -22,6 +22,6 @@ public class ResRecorrido {
         this.fechaI = recorrido.getFechaInicio();
         this.fechaF = recorrido.getFechaFin();
         this.estaActivo = recorrido.getEstaActivo();
-        this.cantTrayectos = recorrido.getTrayectos().size();
+        this.cantTrayectos = recorrido.getCantidadDeTrayectos();
     }
 }
